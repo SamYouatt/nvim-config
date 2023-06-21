6323c22e56b174cadac931849b0480701bb0ba73
SamYouatt <sdyouatt@btinternet.com>
2023-06-21 21:19:34 +0100

    move lualine sections so they actually work and tweak them didn't like file encoding, moved diagnostics to its place

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -2,17 +2,13 @@ require('lualine').setup {
     options = {
         icons_enabled = true,
         theme = 'auto',
-        sections = {
-            lualine_a = { 'mode' },
-            lualine_b = { 'branch', 'diff', 'diagnostics' },
-            lualine_c = { 'filename' },
-            lualine_x = { 'encoding', 'fileformat', 'filetype' },
-            lualine_y = { 'progress' },
-            lualine_z = { 'location' }
-        },
-        tabline = {},
-        winbar = {},
-        inactive_winbar = {},
-        extensions = {}
-    }
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'diagnostics', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
 }
