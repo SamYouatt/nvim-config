5e2ced79132f347954056bca020e74047e63c2a1
SamYouatt <sdyouatt@btinternet.com>
2023-06-27 08:59:50 +0100

    move the cmp mappings so they are actually called stupid boy

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -12,26 +12,8 @@ lsp.set_preferences({
     sign_icons = {}
 })
 
-lsp.on_attach(function(client, bufnr)
-    lsp.buffer_autoformat()
-
-    local opts = { buffer = bufnr, remap = false }
-
-    vim.keymap.set("n", "gd", function() require('telescope.builtin').lsp_definitions() end, opts)
-    vim.keymap.set("n", "gr", function() require('telescope.builtin').lsp_references() end, opts)
-    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrf", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.rename() end, opts)
-    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
-    vim.keymap.set("n", "<leader>ff", function() vim.lsp.buf.format() end, opts)
-end)
-
-lsp.setup()
+-- Fix undefined global 'vim'
+lsp.nvim_workspace()
 
 -- Autocomplete settings
 local cmp = require('cmp')
@@ -53,3 +35,28 @@ local cmp_mappings = lsp.defaults.cmp_mappings({
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-u>'] = cmp.mapping.scroll_docs(4),
 })
+
+lsp.setup_nvim_cmp({
+    mapping = cmp_mappings
+})
+
+lsp.on_attach(function(client, bufnr)
+    lsp.buffer_autoformat()
+
+    local opts = { buffer = bufnr, remap = false }
+
+    vim.keymap.set("n", "gd", function() require('telescope.builtin').lsp_definitions() end, opts)
+    vim.keymap.set("n", "gr", function() require('telescope.builtin').lsp_references() end, opts)
+    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
+    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
+    vim.keymap.set("n", "<leader>vrf", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
+    vim.keymap.set("n", "<leader>ff", function() vim.lsp.buf.format({ timeout_ms = 2000 }) end, opts)
+end)
+
+lsp.setup()
