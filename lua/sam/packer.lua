146694e978d6d4200baf5586854365df004e67a7
SamYouatt <sdyouatt@btinternet.com>
2023-06-30 13:39:35 +0100

    reorganise packer file

--- lua/sam/packer.lua
+++ lua/sam/packer.lua
@@ -1,83 +1,61 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+-- This file can be loaded by calling `lua require("plugins")` from your init.vim
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
-return require('packer').startup(function(use)
+return require("packer").startup(function(use)
     -- Packer can manage itself
-    use 'wbthomason/packer.nvim'
+    use "wbthomason/packer.nvim"
+
+    -- General editor improvements
+    use "theprimeagen/harpoon"
+    use "mbbill/undotree"
+    use "tpope/vim-fugitive"
+    use "tpope/vim-vinegar"
 
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.1',
-        -- or                            , branch = '0.1.x',
-        requires = { { 'nvim-lua/plenary.nvim' } }
+        "nvim-lualine/lualine.nvim",
+        requires = { "nvim-tree/nvim-web-devicons", opt = true }
     }
 
-
-    -- THEMES
-    use({ 'tomasiser/vim-code-dark' })
-
-    use({ 'rose-pine/neovim', as = 'rose-pine' })
-
-    use({ 'shaunsingh/nord.nvim' })
-
-    use({ 'navarasu/onedark.nvim' })
-
-    use({ 'folke/tokyonight.nvim' })
-
-    use({ 'rebelot/kanagawa.nvim' })
-
-    use({ 'catppuccin/nvim' })
-
-    use({ 'mcchrish/zenbones.nvim' })
-    -- END THEMES
-
+    -- Code improvements
+    use "windwp/nvim-ts-autotag"
+    use "jose-elias-alvarez/null-ls.nvim"
+    use "numToStr/Comment.nvim"
+    use "tpope/vim-surround"
 
     use {
-        'nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' }
+        "nvim-telescope/telescope.nvim",
+        requires = { { "nvim-lua/plenary.nvim" } }
     }
 
     use {
-        "windwp/nvim-ts-autotag" }
-
-    use {
-        'theprimeagen/harpoon'
+        "nvim-treesitter/nvim-treesitter",
+        { run = ":TSUpdate" }
     }
 
     use {
-        'mbbill/undotree'
-    }
-
-    use {
-        'tpope/vim-fugitive'
-    }
-
-    use {
-        'VonHeikemen/lsp-zero.nvim',
-        branch = 'v2.x',
+        "VonHeikemen/lsp-zero.nvim",
+        branch = "v2.x",
         requires = {
             -- LSP Support
-            { 'neovim/nvim-lspconfig' }, -- Required
+            { "neovim/nvim-lspconfig" }, -- Required
             {
                 -- Optional
-                'williamboman/mason.nvim',
+                "williamboman/mason.nvim",
                 run = function()
-                    pcall(vim.cmd, 'MasonUpdate')
+                    pcall(vim.cmd, "MasonUpdate")
                 end,
             },
-            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+            { "williamboman/mason-lspconfig.nvim" }, -- Optional
 
             -- Autocompletion
-            { 'hrsh7th/nvim-cmp' },     -- Required
-            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
-            { 'L3MON4D3/LuaSnip' },     -- Required
+            { "hrsh7th/nvim-cmp" },     -- Required
+            { "hrsh7th/cmp-nvim-lsp" }, -- Required
+            { "L3MON4D3/LuaSnip" },     -- Required
         }
 
     }
-
-    use({ "jose-elias-alvarez/null-ls.nvim" })
-
-    use({ 'projekt0n/github-nvim-theme' })
 
     use {
         "windwp/nvim-autopairs",
@@ -86,14 +64,14 @@ return require('packer').startup(function(use)
         end
     }
 
-    use {
-        "nvim-lualine/lualine.nvim",
-        requires = { "nvim-tree/nvim-web-devicons", opt = true }
-    }
-
-    use { "numToStr/Comment.nvim" }
-
-    use { "tpope/vim-surround" }
-
-    use "tpope/vim-vinegar"
+    -- THEMES
+    use "tomasiser/vim-code-dark"
+    use({ "rose-pine/neovim", as = "rose-pine" })
+    use "shaunsingh/nord.nvim"
+    use "navarasu/onedark.nvim"
+    use "folke/tokyonight.nvim"
+    use "rebelot/kanagawa.nvim"
+    use "catppuccin/nvim"
+    use "mcchrish/zenbones.nvim"
+    use "projekt0n/github-nvim-theme"
 end)
