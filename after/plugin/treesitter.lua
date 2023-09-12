f5173031e5d4f9f5343fe88b73bc0c8309965554
SamYouatt <sdyouatt@gmail.com>
2023-09-12 20:29:51 +0100

    add markdown as language

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,7 +1,7 @@
 require 'nvim-treesitter.configs'.setup {
     -- A list of parser names, or "all" (the five listed parsers should always be installed)
     ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "css", "typescript", "rust",
-        "c_sharp", "sql", "json", "toml" },
+        "c_sharp", "sql", "json", "toml", "markdown" },
 
     -- Install parsers synchronously (only applied to `ensure_installed`)
     sync_install = false,
