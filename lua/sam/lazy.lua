e361365640c0a081c66810303e4dfdc6c7c8a22f
SamYouatt <sdyouatt@gmail.com>
2024-01-17 22:24:33 +0000

    harpoon and lualine

--- lua/sam/lazy.lua
+++ lua/sam/lazy.lua
@@ -20,13 +20,55 @@ require("lazy").setup({
     },
 
     -- Editor essentials
+    'theprimeagen/harpoon',
     'tpope/vim-fugitive',
     'tpope/vim-vinegar',
+
+    {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'nvim-tree/nvim-web-devicons', opt = true }
+    },
 
     -- Color scheme
     { 'rose-pine/neovim', name = 'rose-pine', lazy = true },
 })
 
+-- Plugin configs - Will move these later
+-- Fugitive
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
+-- Harpoon
+local mark = require("harpoon.mark")
+local ui = require("harpoon.ui")
+
+vim.keymap.set("n", "<leader>a", mark.add_file)
+vim.keymap.set("n", "<C-m>", ui.toggle_quick_menu)
+
+vim.keymap.set("n", "<C-n>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-e>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-i>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-o>", function() ui.nav_file(4) end)
+
+vim.keymap.set("n", "<C-j>", function() ui.nav_file(1) end)
+vim.keymap.set("n", "<C-k>", function() ui.nav_file(2) end)
+vim.keymap.set("n", "<C-l>", function() ui.nav_file(3) end)
+vim.keymap.set("n", "<C-;>", function() ui.nav_file(4) end)
+
+-- Lualine
+require('lualine').setup {
+    options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = "", right = "" },
+        section_separators = { left = "", right = "" }
+    },
+    sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'diagnostics', 'fileformat', 'filetype' },
+        lualine_y = { 'progress' },
+        lualine_z = { 'location' }
+    },
+}
 vim.cmd.colorscheme('rose-pine')
