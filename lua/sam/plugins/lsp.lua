7162b5e5dd1b1a8d309d1e0e6a9fc4347830049f
SamYouatt <sdyouatt@gmail.com>
2024-01-18 18:47:08 +0000

    set up lsp on attach keybinds all the main ones I use, will probably add more when I remember

--- lua/sam/plugins/lsp.lua
+++ lua/sam/plugins/lsp.lua
@@ -14,9 +14,49 @@ return {
         lspconfig.lua_ls.setup({})
 
         -- Global mappings
-        vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+        vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float) -- when on an error pressing this floats the diagnostic messages
         vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
         vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
-        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist) -- opens the quickfix list with all the diagnostics in the buffer
+
+        -- Configuration that should only run after attaching lsp to buffer
+        vim.api.nvim_create_autocmd('LspAttach', {
+            group = vim.api.nvim_create_augroup('UserLspConfig', {}),
+            callback = function(ev)
+                -- Enable completion triggered by <c-x><c-o>
+                vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
+
+                local opts = { buffer = ev.buf }
+
+                -- Jump to
+                vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+                vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+                vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+                vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+
+                -- Signature info
+                vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+                vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+                vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
+
+                -- Workspace commands
+                vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, opts)
+                vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, opts)
+                vim.keymap.set('n', '<space>wl', function()
+                    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+                end, opts)
+
+                -- Code actions
+                vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
+
+                -- Refactoring
+                vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
+
+                -- Formatting
+                vim.keymap.set('n', '<space>f', function()
+                    vim.lsp.buf.format { async = true }
+                end, opts)
+            end,
+        })
     end
 }
