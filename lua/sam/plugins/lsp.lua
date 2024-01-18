e49318ed63d5ab71f3daea06f68e141709511136
SamYouatt <sdyouatt@gmail.com>
2024-01-18 18:27:23 +0000

    install lsp and mason just lua for now

---
+++ lua/sam/plugins/lsp.lua
@@ -0,0 +1,15 @@
+return {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+    },
+    config = function()
+        require("mason").setup()
+        require("mason-lspconfig").setup()
+
+        local lspconfig = require("lspconfig")
+
+        lspconfig.lua_ls.setup({})
+    end
+}
