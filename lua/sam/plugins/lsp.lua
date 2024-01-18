07148a4479a8ec5a04761486faf210aca86b0094
SamYouatt <sdyouatt@gmail.com>
2024-01-18 18:31:18 +0000

    set up global lsp mappings those that don't need an lsp - diagnostic ones

--- lua/sam/plugins/lsp.lua
+++ lua/sam/plugins/lsp.lua
@@ -1,15 +1,22 @@
 return {
-    "neovim/nvim-lspconfig",
+    'neovim/nvim-lspconfig',
     dependencies = {
-        "williamboman/mason.nvim",
-        "williamboman/mason-lspconfig.nvim",
+        'williamboman/mason.nvim',
+        'williamboman/mason-lspconfig.nvim',
     },
     config = function()
-        require("mason").setup()
-        require("mason-lspconfig").setup()
+        require('mason').setup()
+        require('mason-lspconfig').setup()
 
-        local lspconfig = require("lspconfig")
+        local lspconfig = require('lspconfig')
 
+        -- Setup language servers
         lspconfig.lua_ls.setup({})
+
+        -- Global mappings
+        vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+        vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+        vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+        vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
     end
 }
