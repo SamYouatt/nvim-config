56968619a21d9d648e7ac34e809572d504153706
SamYouatt <sdyouatt@gmail.com>
2024-01-18 18:27:39 +0000

    setup basic treesitter

--- lua/sam/plugins/basic.lua
+++ lua/sam/plugins/basic.lua
@@ -1,12 +1,5 @@
 -- All plugins which don't require any additional setup live in here
 return {
-    {
-        'nvim-treesitter/nvim-treesitter',
-        build = ':TSUpdate'
-    },
-
     'tpope/vim-vinegar',
-
-    -- Color scheme
-    { 'rose-pine/neovim', name = 'rose-pine', lazy = true },
+    { 'rose-pine/neovim', name = 'rose-pine' },
 }
