053892a15f1f2f0175a60684ba236fde23b06783
SamYouatt <sdyouatt@gmail.com>
2024-02-19 19:12:43 +0000

    tried to set up an autocmd to allow set bg for any theme didn't seem to work with tokyonight though worked with github

--- lua/sam/color.lua
+++ lua/sam/color.lua
@@ -1,3 +1,21 @@
 vim.o.termguicolors = true
 
-vim.cmd.colorscheme('tokyonight')
+vim.cmd.colorscheme("tokyonight")
+
+--[[
+vim.api.nvim_create_autocmd({"OptionSet"}, {
+    pattern = {"background"},
+    callback = function(_ev)
+        if vim.o.background == 'dark' then
+            -- Dark theme preference
+            vim.cmd.colorscheme(dark_scheme)
+        else
+            -- light theme preference
+            vim.cmd.colorscheme(light_scheme)
+        end
+
+        -- force full redraw
+        vim.cmd("mode")
+    end
+})
+]]--
