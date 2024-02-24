5b3a64345e3e83a280624da09d638096d6d1254c
SamYouatt <sdyouatt@gmail.com>
2024-02-24 17:16:43 +0000

    move themes out of basic.lua

--- lua/sam/plugins/basic.lua
+++ lua/sam/plugins/basic.lua
@@ -1,23 +1,4 @@
 -- All plugins which don't require any additional setup live in here
 return {
     "tpope/vim-vinegar",
-
-    -- Colourschemes
-    { "rose-pine/neovim",           name = "rose-pine" },
-    { "projekt0n/github-nvim-theme" },
-    {
-        "folke/tokyonight.nvim",
-        config = function()
-            require("tokyonight").setup({
-                style = "night",
-                light_style = "day",
-                styles = {
-                    comments = { italic = true },
-                    keywords = { italic = true},
-                    sidebars = "normal",
-                    floats = "normal",
-                },
-            })
-        end
-    },
 }
---
+++ lua/sam/plugins/themes.lua
@@ -0,0 +1,19 @@
+return {
+	{ "rose-pine/neovim", name = "rose-pine" },
+	{ "projekt0n/github-nvim-theme" },
+	{
+		"folke/tokyonight.nvim",
+		config = function()
+			require("tokyonight").setup({
+				style = "night",
+				light_style = "day",
+				styles = {
+					comments = { italic = true },
+					keywords = { italic = true },
+					sidebars = "normal",
+					floats = "normal",
+				},
+			})
+		end,
+	},
+}
