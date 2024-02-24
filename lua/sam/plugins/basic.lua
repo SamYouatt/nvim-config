fff73b65d02208f18e66b1586fbb4c18f2ca8dcd
SamYouatt <sdyouatt@gmail.com>
2024-02-24 21:09:28 +0000

    add lexima for minimal auto pairs

--- lua/sam/plugins/basic.lua
+++ lua/sam/plugins/basic.lua
@@ -1,4 +1,9 @@
 -- All plugins which don't require any additional setup live in here
 return {
+    -- Improvements to netwr
     "tpope/vim-vinegar",
+    -- Comment commands based on gc
+    "tpope/vim-commentary",
+    -- Minimal auto pairs
+    "cohama/lexima.vim",
 }
