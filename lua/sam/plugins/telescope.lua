7ed3be25f951f4467054ee8f9a5ad5564a0f9796
SamYouatt <sdyouatt@gmail.com>
2024-03-01 17:41:54 +0000

    use native fzf telescope for better performance requires being build but lazy handles that if you provide a build command

--- lua/sam/plugins/telescope.lua
+++ lua/sam/plugins/telescope.lua
@@ -1,13 +1,34 @@
 return {
-        'nvim-telescope/telescope.nvim', tag = '0.1.5',
-        dependencies = { 'nvim-lua/plenary.nvim' },
-        config = function()
-            local builtin = require('telescope.builtin')
-            vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
-            vim.keymap.set('n', '<C-p>', builtin.git_files, {})
-            vim.keymap.set('n', '<leader>ps', function()
-                builtin.grep_string({ search = vim.fn.input("Grep > ") });
-            end)
-            vim.keymap.set('n', '<leader>pg', builtin.live_grep, {})
-        end
+	"nvim-telescope/telescope.nvim",
+	tag = "0.1.5",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+
+		-- native c impletmentation of fzf for faster sorting
+		{
+			"nvim-telescope/telescope-fzf-native.nvim",
+			build = "cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build",
+		},
+	},
+	config = function()
+        require("telescope").setup({
+            extensions = {
+                fzf = {
+                    fuzzy = true,
+                    -- requires telescope-fzf-native.nvim to be built
+                    override_generic_sorter = true,
+                    override_filter_sorter = true,
+                    case_mode = "smart_case",
+                }
+            }
+        })
+
+		local builtin = require("telescope.builtin")
+		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+		vim.keymap.set("n", "<leader>ps", function()
+			builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		end)
+		vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
+	end,
 }
