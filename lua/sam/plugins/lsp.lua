2090ab62fdc2f49d586d91c1bff11d7a0879b000
SamYouatt <sdyouatt@gmail.com>
2024-03-01 17:34:48 +0000

    formatting

--- lua/sam/plugins/lsp.lua
+++ lua/sam/plugins/lsp.lua
@@ -1,67 +1,67 @@
 return {
-    'neovim/nvim-lspconfig',
-    dependencies = {
-        'williamboman/mason.nvim',
-        'williamboman/mason-lspconfig.nvim',
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"williamboman/mason-lspconfig.nvim",
 
-        -- completion plugins
-        'hrsh7th/cmp-nvim-lsp',
-        'hrsh7th/cmp-buffer',
-        'hrsh7th/cmp-path',
-        'hrsh7th/cmp-cmdline',
-        'hrsh7th/nvim-cmp',
-        'L3MON4D3/LuaSnip',
-        'saadparwaiz1/cmp_luasnip',
-    },
-    config = function()
-        require('mason').setup()
-        require('mason-lspconfig').setup({
-            ensure_installed = {
-                'lua_ls',
-                'rust_analyzer',
-                'tsserver',
-                'cssls',
-            }
-        })
+		-- completion plugins
+		"hrsh7th/cmp-nvim-lsp",
+		"hrsh7th/cmp-buffer",
+		"hrsh7th/cmp-path",
+		"hrsh7th/cmp-cmdline",
+		"hrsh7th/nvim-cmp",
+		"L3MON4D3/LuaSnip",
+		"saadparwaiz1/cmp_luasnip",
+	},
+	config = function()
+		require("mason").setup()
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"lua_ls",
+				"rust_analyzer",
+				"tsserver",
+				"cssls",
+			},
+		})
 
-        local lspconfig = require('lspconfig')
+		local lspconfig = require("lspconfig")
 
-        -- Setup language servers
-        lspconfig.lua_ls.setup({})
-        lspconfig.rust_analyzer.setup({})
-        lspconfig.tsserver.setup({})
-        lspconfig.cssls.setup({})
-        lspconfig.eslint.setup({})
+		-- Setup language servers
+		lspconfig.lua_ls.setup({})
+		lspconfig.rust_analyzer.setup({})
+		lspconfig.tsserver.setup({})
+		lspconfig.cssls.setup({})
+		lspconfig.eslint.setup({})
 
-        -- Completion setup
-        local cmp = require('cmp')
+		-- Completion setup
+		local cmp = require("cmp")
 
-        cmp.setup({
-            snippet = {
-                expand = function(args)
-                    require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-            },
-            window = {
-                completion = cmp.config.window.bordered(),
-                documentation = cmp.config.window.bordered(),
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-p>'] = cmp.mapping.select_prev_item(),
-                ['<C-n>'] = cmp.mapping.select_next_item(),
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+				end,
+			},
+			window = {
+				completion = cmp.config.window.bordered(),
+				documentation = cmp.config.window.bordered(),
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-n>"] = cmp.mapping.select_next_item(),
 
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
+				["<C-b>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
 
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
-            }),
-            sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'buffer' },
-                { name = 'luasnip' },
-            })
-        })
-    end
+				["<C-Space>"] = cmp.mapping.complete(),
+				["<C-e>"] = cmp.mapping.abort(),
+				["<CR>"] = cmp.mapping.confirm({ select = true }),
+			}),
+			sources = cmp.config.sources({
+				{ name = "nvim_lsp" },
+				{ name = "buffer" },
+				{ name = "luasnip" },
+			}),
+		})
+	end,
 }
