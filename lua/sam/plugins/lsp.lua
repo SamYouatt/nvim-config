eccfa2a3c63112b38bab45a78738534b74660d24
SamYouatt <sdyouatt@gmail.com>
2024-04-03 21:40:42 +0100

    link to this reddit post in case I need it later

--- lua/sam/plugins/lsp.lua
+++ lua/sam/plugins/lsp.lua
@@ -34,6 +34,7 @@ return {
         lspconfig.eslint.setup({})
         lspconfig.gopls.setup({})
         lspconfig.csharp_ls.setup({})
+        -- super useful reddit post https://www.reddit.com/r/neovim/comments/yukgxy/rust_yew_tailwindcss_intellisense/
         lspconfig.tailwindcss.setup({
             filetypes = {
                 "css",
