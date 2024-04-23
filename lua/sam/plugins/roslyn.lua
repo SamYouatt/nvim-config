d3f0f44b1d683191047fd54e52e2dd5b4425f19c
SamYouatt <sdyouatt@gmail.com>
2024-04-23 11:45:58 +0100

    got it to start but can't get it to attach

--- lua/sam/plugins/roslyn.lua
+++ lua/sam/plugins/roslyn.lua
@@ -1,10 +1,16 @@
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
 return {
     "jmederosalvarado/roslyn.nvim",
     config = function()
 	require("roslyn").setup({
 	    dotnet = "dotnet",
-	    on_attach = function() end,
-	    capabilities = function() end,
+	    on_attach = function(ev)
+		local opts = { buffer = ev.buf }
+		vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+	    end,
+	    capabilities = capabilities,
 	})
     end,
 }
