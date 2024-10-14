f0d029c26e21ea056305c225268e5fd878e1db52
SamYouatt <sdyouatt@gmail.com>
2024-10-14 18:30:06 +0100

    set up harpoon bingings for dusk
    and move all the binds to alt to avoid any conflicts

--- lua/sam/plugins/harpoon.lua
+++ lua/sam/plugins/harpoon.lua
@@ -10,14 +10,16 @@ return {
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
         vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-        vim.keymap.set("n", "<C-n>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-e>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-i>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-o>", function() harpoon:list():select(4) end)
+        -- Dusk
+        vim.keymap.set("n", "<A-h>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<A-a>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<A-e>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<A-i>", function() harpoon:list():select(4) end)
 
-        vim.keymap.set("n", "<C-j>", function() harpoon:list():select(1) end)
-        vim.keymap.set("n", "<C-k>", function() harpoon:list():select(2) end)
-        vim.keymap.set("n", "<C-l>", function() harpoon:list():select(3) end)
-        vim.keymap.set("n", "<C-;>", function() harpoon:list():select(4) end)
+        -- Qwerty
+        vim.keymap.set("n", "<A-j>", function() harpoon:list():select(1) end)
+        vim.keymap.set("n", "<A-k>", function() harpoon:list():select(2) end)
+        vim.keymap.set("n", "<A-l>", function() harpoon:list():select(3) end)
+        vim.keymap.set("n", "<A-;>", function() harpoon:list():select(4) end)
     end
 }
