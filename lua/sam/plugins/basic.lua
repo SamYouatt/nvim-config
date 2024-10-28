26f41bc32ed33edfb6c0e05e7aef398dbaea01ce
SamYouatt <sdyouatt@gmail.com>
2024-10-28 16:24:27 +0000

    formatting

--- lua/sam/plugins/basic.lua
+++ lua/sam/plugins/basic.lua
@@ -1,13 +1,13 @@
 -- All plugins which don't require any additional setup live in here
 return {
-    "tpope/vim-sleuth",
-    "tpope/vim-surround",
-    "mbbill/undotree",
-    "nvim-tree/nvim-web-devicons",
+	"tpope/vim-sleuth",
+	"tpope/vim-surround",
+	"mbbill/undotree",
+	"nvim-tree/nvim-web-devicons",
 
-    { "brenoprata10/nvim-highlight-colors", opts = {} },
-    { "stevearc/dressing.nvim", opts = {} },
-    { "lewis6991/gitsigns.nvim", opts = {} },
+	{ "brenoprata10/nvim-highlight-colors", opts = {} },
+	{ "stevearc/dressing.nvim", opts = {} },
+	-- { "lewis6991/gitsigns.nvim", opts = {} },
 
-    -- { dir = "/Users/samyouatt/Developer/proper-dave", name="proper-dave", opts = {}, config = true },
+	-- { dir = "/Users/samyouatt/Developer/proper-dave", name="proper-dave", opts = {}, config = true },
 }
