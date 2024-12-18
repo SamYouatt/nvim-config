63eccace633f52320b89eb1a11a771dcbbfb3f86
SamYouatt <sdyouatt@gmail.com>
2024-12-18 15:16:01 +0000

    fzflua instead of telescope

---
+++ lua/sam/plugins/fzf_lua.lua
@@ -0,0 +1,16 @@
+return {
+	"ibhagwan/fzf-lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local fzf = require "fzf-lua"
+
+		fzf.setup({})
+
+		vim.keymap.set("n", "<C-p>", fzf.files, {})
+		vim.keymap.set("n", "<leader>ps", function()
+			fzf.grep({ search = vim.fn.input("Grep > ") })
+		end)
+		vim.keymap.set("n", "<leader>pg", fzf.live_grep, {})
+		vim.keymap.set("n", "<leader>ds", fzf.lsp_document_symbols)
+	end
+}
--- lua/sam/plugins/telescope.lua
+++ lua/sam/plugins/telescope.lua
@@ -1,6 +1,7 @@
 return {
 	"nvim-telescope/telescope.nvim",
 	tag = "0.1.5",
+	enabled = false,
 	dependencies = {
 		"nvim-lua/plenary.nvim",
 		-- "nvim-tree/nvim-web-devicons",
@@ -47,12 +48,12 @@ return {
 			},
 		})
 
-		local builtin = require("telescope.builtin")
-		vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
-		vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-		vim.keymap.set("n", "<leader>ps", function()
-			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end)
-		vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
+		-- local builtin = require("telescope.builtin")
+		-- vim.keymap.set("n", "<leader>pf", builtin.find_files, {})
+		-- vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+		-- vim.keymap.set("n", "<leader>ps", function()
+		-- 	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+		-- end)
+		-- vim.keymap.set("n", "<leader>pg", builtin.live_grep, {})
 	end,
 }
