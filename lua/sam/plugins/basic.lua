c92ede44c8c9b0e4d4f72b55df8a005033bc80a7
Sam Youatt <sdyouatt@gmail.com>
2025-02-11 15:47:40 +0000

    remove vim-surround, using mini surround now

--- lua/sam/plugins/basic.lua
+++ lua/sam/plugins/basic.lua
@@ -1,9 +1,7 @@
 -- All plugins which don't require any additional setup live in here
 return {
 	"tpope/vim-sleuth",
-	"tpope/vim-surround",
 	"tpope/vim-fugitive",
 	{ "j-hui/fidget.nvim",                  opts = {} },
 	{ "brenoprata10/nvim-highlight-colors", opts = {} },
-	{ "folke/snacks.nvim",                  opts = { input = {} } },
 }
