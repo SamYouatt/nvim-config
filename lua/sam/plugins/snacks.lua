4732684c8cd6dbbae803911f304d9e424c044484
Sam Youatt <sdyouatt@gmail.com>
2025-02-11 15:51:47 +0000

    don't want this smart picker

--- lua/sam/plugins/snacks.lua
+++ lua/sam/plugins/snacks.lua
@@ -8,7 +8,6 @@ return {
 		picker = { enabled = true },
 	},
 	keys = {
-		{ "<leader><space>", function() Snacks.picker.smart() end,                                            desc = "Smart Find Files" },
 		{ "<C-p>",           function() Snacks.picker.git_files({ untracked = true, submodules = true }) end, desc = "Git files" },
 		{ "<leader>pg",      function() Snacks.picker.grep() end,                                             desc = "Live grep" },
 		{ "<leader>ds",      function() Snacks.picker.lsp_symbols() end,                                      desc = "Document symbols" },
