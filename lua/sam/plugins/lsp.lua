45bf0032c92bb74bdc4279026facfa4ed39e8d2f
Sam Youatt <sdyouatt@gmail.com>
2025-03-21 22:04:35 +0000

    I don't like the icons
    they aren't consistent in size which is annoying me

--- lua/sam/plugins/lsp.lua
+++ lua/sam/plugins/lsp.lua
@@ -76,10 +76,10 @@ return {
             vim.diagnostic.config {
                 signs = {
                     text = {
-                        [vim.diagnostic.severity.ERROR] = "",
-                        [vim.diagnostic.severity.WARN] = "",
-                        [vim.diagnostic.severity.INFO] = "",
-                        [vim.diagnostic.severity.HINT] = "",
+                        [vim.diagnostic.severity.ERROR] = "E",
+                        [vim.diagnostic.severity.WARN] = "W",
+                        [vim.diagnostic.severity.INFO] = "I",
+                        [vim.diagnostic.severity.HINT] = "H",
                     }
                 }
             }
