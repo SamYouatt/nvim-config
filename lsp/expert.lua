b4b2acb4f8e4f1e3932224011caf19d0111f5c04
Sam Youatt <samyouatt@Sams-MacBook-Pro.local>
2025-11-04 08:37:29 +0000

    update expert but still not working

--- lsp/expert.lua
+++ lsp/expert.lua
@@ -1,7 +1,18 @@
+---@brief
+---
+--- https://github.com/elixir-lang/expert
+---
+--- Expert is the official language server implementation for the Elixir programming language.
+---
+--- 'root_dir' is chosen like this: if two or more directories containing `mix.exs` were found when
+--- searching directories upward, the second one (higher up) is chosen, with the assumption that it
+--- is the root of an umbrella app. Otherwise the directory containing the single mix.exs that was
+--- found is chosen.
+
 ---@type vim.lsp.Config
 return {
   filetypes = { 'elixir', 'eelixir', 'heex', 'surface' },
-  cmd = { 'expert' },
+  cmd = { 'expert', '--stdio' },
   root_dir = function(bufnr, on_dir)
     local fname = vim.api.nvim_buf_get_name(bufnr)
     --- Elixir workspaces may have multiple `mix.exs` files, for an "umbrella" layout or monorepo.
