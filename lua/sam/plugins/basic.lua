32b10d3c7430e3aadd9e0db2b79a6bb9e340531b
Sam Youatt <sdyouatt@gmail.com>
2026-05-31 18:15:02 +0100

    nvim: jump between diff hunks

--- lua/sam/plugins/basic.lua
+++ lua/sam/plugins/basic.lua
@@ -1,8 +1,32 @@
+-- test
 -- All plugins which don't require any additional setup live in here
 return {
 	"tpope/vim-sleuth",
 	"tpope/vim-fugitive",
-	"lewis6991/gitsigns.nvim",
+	{
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			on_attach = function(bufnr)
+				local gitsigns = require("gitsigns")
+
+				vim.keymap.set("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.nav_hunk("next")
+					end
+				end, { buffer = bufnr, desc = "Next git hunk" })
+
+				vim.keymap.set("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
+					end
+				end, { buffer = bufnr, desc = "Previous git hunk" })
+			end,
+		},
+	},
 	"sindrets/diffview.nvim",
 	{ "j-hui/fidget.nvim",                  opts = {} },
 	{ "brenoprata10/nvim-highlight-colors", opts = {} },
