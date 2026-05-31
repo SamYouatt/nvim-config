89bf98f480acfdbed912e2cb5e6213deadcd0f3f
Sam Youatt <sdyouatt@gmail.com>
2026-05-31 18:03:29 +0100

    nvim: install fff

---
+++ lua/sam/plugins/fff.lua
@@ -0,0 +1,22 @@
+return {
+	"dmtrKovalenko/fff.nvim",
+	build = function()
+		require("fff.download").download_or_build_binary()
+	end,
+	lazy = false,
+	opts = {
+		base_path = vim.fn.getcwd(),
+		layout = {
+			anchor = "bottom",
+			height = 0.4,
+			prompt_position = "bottom",
+			width = 1,
+		},
+	},
+	keys = {
+		{ "<C-p>", function() require("fff").find_files() end, desc = "Git files" },
+		{ "<leader>pg", function() require("fff").live_grep({ query = "!dist/ " }) end, desc = "Live grep" },
+		{ "<leader>pf", function() require("fff").find_files() end, desc = "Find files" },
+		{ "gO", vim.lsp.buf.document_symbol, desc = "LSP outline" },
+	},
+}
--- lua/sam/plugins/snacks.lua
+++ lua/sam/plugins/snacks.lua
@@ -4,12 +4,5 @@ return {
 	lazy = false,
 	opts = {
 		input = { enabled = true },
-		picker = { enabled = true, layout = { preset = "ivy", layout = { position = "bottom" }, }, },
-	},
-	keys = {
-		{ "<C-p>",      function() Snacks.picker.git_files({ untracked = true, submodules = true }) end, desc = "Git files" },
-		{ "<leader>pg", function() Snacks.picker.grep({ exclude = { "dist/" } }) end,                    desc = "Live grep" },
-		{ "<leader>pf", function() Snacks.picker.files({ cwd = vim.fn.getcwd() }) end,                   desc = "Find files" },
-		{ "gO",         function() Snacks.picker.lsp_symbols() end,                                      desc = "LSP outline" },
 	},
 }
