905457727c1ede047d1ff659ac780df301c8b20d
Sam Youatt <sdyouatt@gmail.com>
2026-05-31 18:06:58 +0100

    nvim: shortcut for find word under cursor

--- lua/sam/plugins/fff.lua
+++ lua/sam/plugins/fff.lua
@@ -15,6 +15,7 @@ return {
 	},
 	keys = {
 		{ "<C-p>", function() require("fff").find_files() end, desc = "Git files" },
+		{ "<leader>fw", function() require("fff").live_grep({ query = vim.fn.expand("<cword>") }) end, desc = "Find word" },
 		{ "<leader>pg", function() require("fff").live_grep({ query = "!dist/ " }) end, desc = "Live grep" },
 		{ "<leader>pf", function() require("fff").find_files() end, desc = "Find files" },
 		{ "gO", vim.lsp.buf.document_symbol, desc = "LSP outline" },
