75a53a96d2a89504f8ef72f93855b8903804f904
Sam Youatt <sdyouatt@gmail.com>
2026-05-31 18:12:48 +0100

    nvim: fff find modified

--- lua/sam/plugins/fff.lua
+++ lua/sam/plugins/fff.lua
@@ -6,6 +6,9 @@ return {
 	lazy = false,
 	opts = {
 		base_path = vim.fn.getcwd(),
+		git = {
+			status_text_color = true,
+		},
 		layout = {
 			anchor = "bottom",
 			height = 0.4,
@@ -14,10 +17,11 @@ return {
 		},
 	},
 	keys = {
-		{ "<C-p>", function() require("fff").find_files() end, desc = "Git files" },
+		{ "<C-p>",      function() require("fff").find_files() end,                                    desc = "Git files" },
+		{ "<leader>fm", function() require("fff").find_files({ query = "git:modified " }) end,         desc = "Modified files" },
 		{ "<leader>fw", function() require("fff").live_grep({ query = vim.fn.expand("<cword>") }) end, desc = "Find word" },
-		{ "<leader>pg", function() require("fff").live_grep({ query = "!dist/ " }) end, desc = "Live grep" },
-		{ "<leader>pf", function() require("fff").find_files() end, desc = "Find files" },
-		{ "gO", vim.lsp.buf.document_symbol, desc = "LSP outline" },
+		{ "<leader>pg", function() require("fff").live_grep({ query = "!dist/ " }) end,                desc = "Live grep" },
+		{ "<leader>pf", function() require("fff").find_files() end,                                    desc = "Find files" },
+		{ "gO",         vim.lsp.buf.document_symbol,                                                   desc = "LSP outline" },
 	},
 }
