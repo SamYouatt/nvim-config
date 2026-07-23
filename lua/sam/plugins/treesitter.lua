c5e3af80217ecd9162573207a73b56f443af22bd
Sam Youatt <sdyouatt@gmail.com>
2026-07-23 16:30:04 +0100

    update treesitter to main branch

--- lua/sam/plugins/treesitter.lua
+++ lua/sam/plugins/treesitter.lua
@@ -1,38 +1,65 @@
+local parsers = {
+    "c",
+    "c_sharp",
+    "css",
+    "html",
+    "javascript",
+    "json",
+    "lua",
+    "markdown",
+    "markdown_inline",
+    "query",
+    "rust",
+    "sql",
+    "toml",
+    "typescript",
+    "vim",
+    "vimdoc",
+}
+
 return {
     "nvim-treesitter/nvim-treesitter",
+    branch = "main",
+    lazy = false,
     build = ":TSUpdate",
     dependencies = {
-        "nvim-treesitter/nvim-treesitter-textobjects",
+        {
+            "nvim-treesitter/nvim-treesitter-textobjects",
+            branch = "main",
+        },
     },
     config = function()
-        local configs = require("nvim-treesitter.configs")
+        local treesitter = require("nvim-treesitter")
 
-        configs.setup({
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "javascript", "html", "css", "typescript", "rust",
-                "c_sharp", "sql", "json", "toml", "markdown" },
-
-            sync_install = true,
-            auto_install = true,
-
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ["aa"] = "@parameter.outer",
-                        ["ia"] = "@parameter.inner",
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer",
-                        ["ic"] = "@class.inner",
-                    }
-                }
-            }
+        treesitter.setup({
+            install_dir = vim.fn.stdpath("data") .. "/site",
         })
-    end
+        treesitter.install(parsers)
+
+        vim.api.nvim_create_autocmd("FileType", {
+            callback = function()
+                pcall(vim.treesitter.start)
+            end,
+        })
+
+        require("nvim-treesitter-textobjects").setup({
+            select = {
+                lookahead = true,
+            },
+        })
+
+        local select = require("nvim-treesitter-textobjects.select")
+        for lhs, query in pairs({
+            aa = "@parameter.outer",
+            ia = "@parameter.inner",
+            af = "@function.outer",
+            ["if"] = "@function.inner",
+            ac = "@class.outer",
+            ic = "@class.inner",
+        }) do
+            vim.keymap.set({ "x", "o" }, lhs, function()
+                select.select_textobject(query, "textobjects")
+            end)
+        end
+    end,
 }
