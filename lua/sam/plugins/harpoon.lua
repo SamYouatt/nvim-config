96f5c3efca926b0550480f4975086b91f12f903a
Sam Youatt <sdyouatt@gmail.com>
2026-08-07 11:43:12 +0100

    change harpoon binding cos it conflicted with herdr

--- lua/sam/plugins/harpoon.lua
+++ lua/sam/plugins/harpoon.lua
@@ -8,7 +8,7 @@ return {
         harpoon:setup()
 
         vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
-        vim.keymap.set("n", "<C-h>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+        vim.keymap.set("n", "<leader>hq", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
         -- Dusk
         vim.keymap.set("n", "<A-h>", function() harpoon:list():select(1) end)
